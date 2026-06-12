5bc5fc953c1b96949b21314a1901facbaabfddd3
basit <fikayobasit@gmail.com>
2026-06-12 21:21:12 +0100

    feat: add DeveloperProfile class

---
+++ src/DeveloperProfile.java
@@ -0,0 +1,8 @@
+public class DeveloperProfile {
+    public static void main(String[] args) {
+        System.out.println("Name: Basit");
+        System.out.println("Job Title: Junior Java Developer");
+        System.out.print("Status: ");
+        System.out.print("Learning Java - Day 1");
+    }
+}
