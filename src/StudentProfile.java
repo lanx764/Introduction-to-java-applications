0c6333dd2326096bde4e2b2886be4e4a6231b111
basit <fikayobasit@gmail.com>
2026-06-12 21:54:44 +0100

    feat: add StudentProfile class

---
+++ src/StudentProfile.java
@@ -0,0 +1,8 @@
+public class StudentProfile {
+    public static void main(String[] args) {
+        System.out.println("Student: Basit");
+        System.out.println("Course: Java Development");
+        System.out.println("Year: 2026");
+        System.out.println("Status: Active");
+    }
+}
