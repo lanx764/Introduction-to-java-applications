ba0a56ca9bfc645f98d820916a70d9258dc3f286
basit <fikayobasit@gmail.com>
2026-06-13 07:30:35 +0100

    feat: add InvoiceHeader class

---
+++ src/InvoiceHeader.java
@@ -0,0 +1,14 @@
+public class InvoiceHeader {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("\tBASIT TECH SOLUTIONS");
+        System.out.println("=================================");
+        System.out.println("Client: \tAnthropic Inc.");
+        System.out.println("Service: \tJava Development");
+        System.out.println("Quote: \t\"Premium Package\"");
+        System.out.println("Files at: \tC:\\invoices\\2026");
+        System.out.print("=================================");
+
+
+    }
+}
